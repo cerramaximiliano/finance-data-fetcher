c6df681bb811c340f7c34854b39ec448786073ef
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-07-26 07:13:39 -0300

    features/ controllers

--- app.js
+++ app.js
@@ -1,20 +1,102 @@
 const dotenv = require("dotenv");
-dotenv.config();
+  dotenv.config();
+const fs = require("fs");
+const path = require("path");
 const {
   fetchEconomicCalendar,
   fetchEarningCalendar,
   fetchDayWath,
+  fetchStockPrice,
 } = require("./server/controllers/controllersAPIs");
 
 (async () => {
+  const openSymbols = [
+    { description: "Futuros Bonos US 10 años", symbol: "ZN=F" },
+    { description: "Futuros Soja", symbol: "ZS=F" },
+    { description: "Futuros Oro", symbol: "GC=F" },
+    { description: "Futuros Plata", symbol: "SI=F" },
+    { description: "Futuros Petróleo", symbol: "CL=F" },
+    { description: "Futuros S&P 500", symbol: "ES=F" },
+    { description: "Futuros NASDAQ 100", symbol: "NQ=F" },
+    { description: "Futuros Dow Jones", symbol: "YM=F" },
+    { description: "Futuros Russell 2000", symbol: "RTY=F" },
+    { description: "Futuros Dólar Îndex", symbol: "DX=F" },
+    { description: "Bitcoin/USD", symbol: "BTC-USD" },
+    { description: "Etherum/USD", symbol: "ETH-USD" },
+  ];
+
+  const closeSymbols = [
+    { description: "S&P 500", symbol: "^SPX" },
+    { description: "Nasdaq", symbol: "^IXIC" },
+    { description: "Dow Jones", symbol: "^DJI" },
+    { description: "Russell 2000", symbol: "^RUT" },
+    { description: "Tasa Bonos US 10 años ", symbol: "^TNX" },
+    { description: "DAX", symbol: "^GDAXI", country: "Germany" },
+    { description: "SSE", symbol: "000001.SS", country: "China" },
+    { description: "Nikkei", symbol: "^N225" },
+    { description: "Bovespa", symbol: "^BVSP" },
+    { description: "Merval", symbol: "^MERV" },
+    { description: "US Dólar Index", symbol: "DX-Y.NYB" },
+    { description: "Futuros Soja", symbol: "ZS=F" },
+    { description: "Futuros Oro", symbol: "GC=F" },
+    { description: "Futuros Plata", symbol: "SI=F" },
+    { description: "Futuros Petróleo", symbol: "CL=F" },
+    { description: "Bitcoin/USD", symbol: "BTC-USD" },
+    { description: "Etherum/USD", symbol: "ETH-USD" },
+  ];
+
   try {
-    const economicCalendar = await fetchEconomicCalendar();
-    console.log(economicCalendar);
+
+/*     const economicCalendar = await fetchEconomicCalendar("2024-01-01", "2024-12-31", ["Interest Rate","Inflation Rate"]);
+    const filePath = path.join(
+      __dirname,
+      "server",
+      "examples",
+      "investing",
+      "economicCalendar.json"
+    );
+    fs.writeFileSync(filePath, JSON.stringify(economicCalendar, null, 2));  */
+
+
+
     const earningCalendar = await fetchEarningCalendar();
-    console.log(earningCalendar.data)
-    const dayWatch = await fetchDayWath();
-    console.log(dayWatch);
+    
+
+
+/*     const dayWatch = await fetchDayWath();
+    console.log(dayWatch); */
+    
+    
+    /* const openMarketPromises = openSymbols.map((symbols) => {
+      return fetchStockPrice(symbols.symbol);
+    });
+    const openMarketResults = await Promise.all(openMarketPromises);
+    const openMarketData = openMarketResults.map((result) => result.data);
+    const filePath = path.join(
+      __dirname,
+      "server",
+      "examples",
+      "yahooFinance",
+      "openMarketData.json"
+    );
+    fs.writeFileSync(filePath, JSON.stringify(openMarketData, null, 2)); */
+
+/*     const closeMarketPromises = closeSymbols.map((symbols) => {
+      return fetchStockPrice(symbols.symbol);
+    });
+    const closeMarketResults = await Promise.all(closeMarketPromises);
+    const closeMarketData = closeMarketResults.map((result) => result.data);
+    const filePathClose = path.join(
+      __dirname,
+      "server",
+      "examples",
+      "yahooFinance",
+      "closeMarketData.json"
+    );
+    fs.writeFileSync(filePathClose, JSON.stringify(closeMarketData, null, 2)); */
+
+
   } catch (error) {
-    console.error(error.message);
+    console.error(`error: ${error.message}`);
   }
 })();
--- server/controllers/controllersAPIs.js
+++ server/controllers/controllersAPIs.js
@@ -6,45 +6,56 @@ const {
   getUnixStartOfDay,
   getUnixEndOfDay,
 } = require("../utils/dates");
-const { rapidApiKey, rapidApiTradingViewHost, rapidApiSeekingAlphaHost } = require("../config/configAPIs");
+const {
+  rapidApiKey,
+  rapidApiTradingViewHost,
+  rapidApiSeekingAlphaHost,
+  rapidApiYahooFinance,
+} = require("../config/configAPIs");
 // Tradign View
-const fetchEconomicCalendar = async () => {
-  const today = getToday();
-  const tomorrow = getTomorrow();
-
+const fetchEconomicCalendar = async (
+  from = getToday(),
+  to = getTomorrow(),
+  indicatorFilter = [],
+  minImportance = "1"
+) => {
   const options = {
     method: "GET",
     url: "https://trading-view.p.rapidapi.com/calendars/get-economic-calendar",
     params: {
-      from: today,
-      to: tomorrow,
+      from: from,
+      to: to,
       countries: "US",
       lang: "en",
+      minImportance: minImportance,
     },
     headers: {
       "x-rapidapi-key": rapidApiKey,
       "x-rapidapi-host": rapidApiTradingViewHost,
     },
   };
-
   try {
     const response = await axios.request(options);
-    return response.data;
+    const { result } = response.data;
+    const uniqueIndicators = [...new Set(result.map(item => item.indicator))];
+    let filterData = result
+    if (indicatorFilter.length > 0){
+      filterData = result.filter(item => indicatorFilter.includes(item.indicator));
+    }
+    return {data: filterData, uniqueIndicators: uniqueIndicators};
   } catch (error) {
     throw new Error(`Error fetching data: ${error.message}`);
   }
 };
 
-const fetchEarningCalendar = async () => {
-  const startOfDay = getUnixStartOfDay();
-  const endOfDay = getUnixEndOfDay();
-  console.log(startOfDay, endOfDay)
+const fetchEarningCalendar = async (from = getUnixStartOfDay(), to = getUnixEndOfDay() ) => {
+  console.log(from, to)
   const options = {
     method: "GET",
     url: "https://trading-view.p.rapidapi.com/calendars/get-earning-calendar",
     params: {
-      from: startOfDay,
-      to: endOfDay,
+      from: from,
+      to: to,
       screenerName: "america",
       lang: "en",
     },
@@ -64,43 +75,59 @@ const fetchEarningCalendar = async () => {
 
 // Seek Alpha
 const fetchDayWath = async () => {
-    const options = {
-        method: 'GET',
-        url: 'https://seeking-alpha.p.rapidapi.com/market/get-day-watch',
-        headers: {
-          'x-rapidapi-key': rapidApiKey,
-          'x-rapidapi-host': rapidApiSeekingAlphaHost
-        }
-      };
-      try {
-        const response = await axios.request(options);
-        return response.data;
-    } catch (error) {
-        throw new Error(`Error fetching day watch data: ${error.message}`);;
-    }
-}
+  const options = {
+    method: "GET",
+    url: "https://seeking-alpha.p.rapidapi.com/market/get-day-watch",
+    headers: {
+      "x-rapidapi-key": rapidApiKey,
+      "x-rapidapi-host": rapidApiSeekingAlphaHost,
+    },
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching day watch data: ${error.message}`);
+  }
+};
 
 const marketOpen = async () => {
-    const options = {
-        method: 'GET',
-        url: 'https://seeking-alpha.p.rapidapi.com/market/get-market-open',
-        headers: {
-          'x-rapidapi-key': rapidApiKey,
-          'x-rapidapi-host': rapidApiSeekingAlphaHost
-        }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          return response.data
-      } catch (error) {
-        throw new Error(`Error fetching market open data: ${error.message}`);;
-      }
-}
+  const options = {
+    method: "GET",
+    url: "https://seeking-alpha.p.rapidapi.com/market/get-market-open",
+    headers: {
+      "x-rapidapi-key": rapidApiKey,
+      "x-rapidapi-host": rapidApiSeekingAlphaHost,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching market open data: ${error.message}`);
+  }
+};
+
+/* Yahoo Finance */
+const fetchStockPrice = async (symbol) => {
+  const options = {
+    method: "POST",
+    url: "https://yahoo-finance160.p.rapidapi.com/info",
+    headers: {
+      "x-rapidapi-key": "621ed88709msh98d154a956d1ac8p1048b4jsn33f22e7820db",
+      "x-rapidapi-host": rapidApiYahooFinance,
+      "Content-Type": "application/json",
+    },
+    data: { stock: symbol },
+  };
+  return axios.request(options);
+};
 
 module.exports = {
   fetchEconomicCalendar,
   fetchEarningCalendar,
   fetchDayWath,
-  marketOpen
+  marketOpen,
+  fetchStockPrice,
 };
