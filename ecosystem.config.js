c0d76d9d7e655dd4d1f770199cef8088bee443d6
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-08-05 14:58:38 -0300

    fix/ ecosystem flie

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,18 @@
 // ecosystem.config.js
 module.exports = {
-    apps: [
-      {
-        name: "finance-data-fetcher",
-        script: "server/app.js",
-        watch: true,
-        env: {
-          PORT: 8085,
-          NODE_ENV: "development"
-        },
-        env_production: {
-          NODE_ENV: "production"
-        }
+  apps: [
+    {
+      name: "finance-data-fetcher",
+      script: "app.js",
+      watch: true,
+      ignore_watch: ["node_modules", "server/logs"], // Excluye la carpeta de logs y node_modules
+      env: {
+        PORT: 8085,
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
       }
-    ]
-  };
+    }
+  ]
+};
